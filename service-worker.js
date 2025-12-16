42fee2f8c3e434450153231daa1d3f2421a0b549
MercyMobile <cisco.velez76@gmail.com>
2025-12-16 13:10:02 -0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+// service-worker.js
+
+const CACHE_NAME = 'nephilim-wars-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  // Cache the CDN libraries your app relies on
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://unpkg.com/lucide@latest/dist/umd/lucide.min.js'
+  // Add your local images here if you want them offline available
+  // './images/Ohya.png',
+  // './images/Azazel.jpg', etc.
+];
+
+// Install Event: Cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event: Serve from cache, fall back to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached response if found, else fetch from network
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
