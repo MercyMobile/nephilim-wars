511f53ff5948cb0623fb3680f230621f5a496c9d
Halonan <cisco.velez76@gmail.com>
2025-12-30 03:09:36 -0800

    Fix: Remove undefined token parameter from creator iframe

--- src/components/CreatorScreen.jsx
+++ src/components/CreatorScreen.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const CreatorScreen = () => {
-  const token = import.meta.env.VITE_HF_TOKEN;
-  
   return (
     <div className="fixed inset-0 z-40 bg-stone-900 w-full h-full">
       <iframe 
-        src={`/creator.html?token=${token}`}
+        src="/creator.html"  // Remove the token parameter
         title="Character Creator"
         className="w-full h-full border-none block"
         style={{ width: '100vw', height: '100vh' }} 
